8bee6e75d0ebfcf6e13263d9292e97e1e402aa2e
yangxia605 <yangxia605@gmail.com>
2018-03-13 22:06:07 +0800

    add Yelp Util to request data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import React from 'react';
 import './App.css';
+import Yelp from'./util/Yelp';
 
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Bordertown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Bordertown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+//
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ]
 
 
 class App extends React.Component {
-  searchYelp(term, location, sortBy){
-    console.log(`Searching Yelp with ${term}, ${location},${sortBy}`)
+  constructor(props){
+    super(props)
+
+    this.state={
+      business: []
+    }
+
+    this.searchYelp=this.searchYelp.bind(this);
   }
-  // constructor(props){
-  //   super(props)
-  //
-  // }
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp}/>
-        <BusinessList businesses={businesses}/>
+        <BusinessList businesses={this.state.business}/>
       </div>
     );
   }
